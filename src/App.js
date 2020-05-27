97881232d897e2e804c348c2ea1ce08576bc4658
Youssef Elwakil <ysefwakil@gmail.com>
2020-05-27 06:59:13 +0000

    Google Maps Implementation v1.1
    Forgot Semi Colon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from './components/AppHeader';
 import AppContainer from './containers/AppContainer';
-import MapContainer from "./containers/MapContainer"
+import MapContainer from './containers/MapContainer';
 import './App.css';
 
 function App() {
