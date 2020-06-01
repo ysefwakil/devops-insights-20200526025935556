6bd531c657b6f54493ee7ebd451e8191d9bcdf92
Youssef Elwakil <ysefwakil@gmail.com>
2020-06-01 04:03:30 +0000

    Testing google API

--- src/App.js
+++ src/App.js
@@ -11,6 +11,45 @@ function App() {
       <AppHeader />
       <AppContainer />
     </div>
+    <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+        
+        #map {
+            height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="map"></div>
+    <script>
+        var map;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: -38.488245,
+                    lng: 175.683139
+                },
+                zoom: 7
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgKaNdZS-JOhCKOw8PYv3jJVTYqFU6Soo&callback=initMap" async defer></script>
+</body>
   );
 }
 
