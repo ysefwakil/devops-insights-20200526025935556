b66640f5a2b64260f2886e274ce71d5b29847e79
Youssef Elwakil <ysefwakil@gmail.com>
2020-06-02 03:17:17 +0000

    Fixing testing for part 1

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -6,7 +6,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -14,7 +14,7 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&q=' + zip;
 
 	request({
 		method: 'GET',
@@ -26,7 +26,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -69,34 +69,34 @@ router.get('/getWeather', exports.getWeather);
 //};
 //router.get('/getWeather2', exports.getWeather2);
 
-exports.getWeather3 = function(req, res) {
-	var zip = req.query.zip;
-	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
-	}
-
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
-
-	request({
-		method: 'GET',
-        url: aurl,
-  		json: true
-    }, function(err, resp, body) {
-    	if(err) {
-    		res.status(400).send('Failed to get the data');
-    		//console.error("Failed to send request to openweathermap.org", err);
-    	} else {
-    		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
-    			var response = {city: body.name, weather: weath};
-    			return res.status(200).send(response);
-    		} else {
-                return res.status(400).send({msg:'Failed'});
-            }
-    	}
-    });
-
-};
-router.get('/getWeather3', exports.getWeather3);
+//exports.getWeather3 = function(req, res) {
+//	var zip = req.query.zip;
+//	if( (zip === null) || (typeof(zip) === 'undefined') ) {
+//		return res.status(400).send('zip missing');
+//	}
+//
+//	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+//
+//	request({
+//		method: 'GET',
+//        url: aurl,
+//  		json: true
+//    }, function(err, resp, body) {
+//    	if(err) {
+//    		res.status(400).send('Failed to get the data');
+//    		//console.error("Failed to send request to openweathermap.org", err);
+//    	} else {
+//    		if(body.cod === 200) {
+//    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+//    			var response = {city: body.name, weather: weath};
+//    			return res.status(200).send(response);
+//    		} else {
+//                return res.status(400).send({msg:'Failed'});
+//            }
+//    	}
+//    });
+//
+//};
+//router.get('/getWeather3', exports.getWeather3);
 
 exports.router = router;
