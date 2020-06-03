9a0c61ee0ff4720c46f437f55e04255faae0eb0d
Youssef <ysefwakil@gmail.com>
2020-06-04 06:12:51 +1200

    Part 2 completed without test - FINALLY

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from './components/AppHeader';
 import AppContainer from './containers/AppContainer';
-import AppMap from './components/AppMap';
+
 
 import './App.css';
 
@@ -10,7 +10,6 @@ function App() {
   return (
 
     <div className="App">
-      <AppMap />
       <AppHeader />
       <AppContainer />
     </div>
