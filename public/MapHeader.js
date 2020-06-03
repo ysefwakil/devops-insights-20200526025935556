5006413331e0f665d823f4d4f0c15fe110b75ff4
Youssef <ysefwakil@gmail.com>
2020-06-04 10:55:04 +1200

    COMPX341 Version Completed

--- public/MapHeader.js
+++ public/MapHeader.js
@@ -1,44 +1,43 @@
-
-function initMap(){
-
+function initMap() {
 
 
-  var options = {
-    zoom: 7,
-    center:{lat:-36.8485,lng:174.7633}
-  }
-  var map = new google.maps.Map(document.getElementById('map'), options);
+
+    var options = {
+        zoom: 7,
+        center: { lat: -36.8485, lng: 174.7633 }
+    }
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
 
-  google.maps.event.addListener(map, 'click', function(event){
-    addMarker({coords:event.latLng});
+    google.maps.event.addListener(map, 'click', function(event) {
+        addMarker({ coords: event.latLng });
 
 
-  });
-  addMarker({lat:tempLat,lng:tempLng});
-  function addMarker(props){
-    var marker = new google.maps.Marker({
-      position:props.coords,
-      map:map
     });
+    addMarker({ lat: tempLat, lng: tempLng });
 
-    marker.addListener('click', function() {
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+        });
 
-      var lat = this.position.lat();
-      var lng = this.position.lng();
-      localStorage.setItem('lat', lat);
-      localStorage.setItem('lng', lng);
-      const Http = new XMLHttpRequest();
-      const url = 'https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&lat=' +lat + '&lon=' +lng;
-      Http.open("GET", url);
-      Http.send();
-      
-      Http.onreadystatechange = (e) => {
-        localStorage.setItem('weather', Http.responseText);
-      }
-    });
-  }
+        marker.addListener('click', function() {
+
+            var lat = this.position.lat();
+            var lng = this.position.lng();
+            localStorage.setItem('lat', lat);
+            localStorage.setItem('lng', lng);
+            const Http = new XMLHttpRequest();
+            const url = 'https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&lat=' + lat + '&lon=' + lng;
+            Http.open("GET", url);
+            Http.send();
+
+            Http.onreadystatechange = (e) => {
+                localStorage.setItem('weather', Http.responseText);
+                document.getElementById('usr').value = JSON.parse(Http.responseText).name;
+            }
+        });
+    }
 
 }
-
-
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -7,15 +7,16 @@ function AppContainer(props) {
 
     const [responseData, setResponseData] = useState('');
 
-    const handleZipChange = async (zipValue) => {
+    const handleZipChange = async(zipValue) => {
         //console.log(`--------- fetchData called zip:${zipValue}`)
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${zipValue}`)
 
 
         const json = await res.json()
 
-        setResponseData(json);
+
         localStorage.setItem('weather', JSON.stringify(json));
+        setResponseData(json);
 
     }
 
@@ -24,23 +25,32 @@ function AppContainer(props) {
     }
 
     return (
-        
-        <div>
-            
-            
-            <div className="row mt-4">
-                <div className="col-sm-4"></div>
-                <Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
-                <div className="col-sm-4"></div>
-            </div>
 
-            <div className="row mt-4a">
-                <div className="col-sm-2"></div>
-                <ZipResponse responseData= {JSON.parse(localStorage.getItem('weather'))} clearResponse={clearResponse}/>
-                <div className="col-sm-2"></div>
-            </div>
-        </div>
-        
+        <
+        div >
+
+
+        <
+        div className = "row mt-4" >
+        <
+        div className = "col-sm-4" > < /div> <
+        Zip onZipChange = { handleZipChange }
+        clearResponse = { clearResponse }
+        /> <
+        div className = "col-sm-4" > < /div> <
+        /div>
+
+        <
+        div className = "row mt-4a" >
+        <
+        div className = "col-sm-2" > < /div> <
+        ZipResponse responseData = { JSON.parse(localStorage.getItem('weather')) }
+        clearResponse = { clearResponse }
+        /> <
+        div className = "col-sm-2" > < /div> <
+        /div> <
+        /div>
+
     );
 }
 
